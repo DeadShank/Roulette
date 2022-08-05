7dc52acacbd43c3cac0c8380b6c975ccfd0f187f
Alexander <saniareva@gmail.com>
2022-08-05 13:05:05 +0300

    update logic

---
+++ Roulette/LoginVC.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Roulette
+//
+//  Created by Alex on 04.08.2022.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    @IBOutlet weak var nameTF: UITextField!
+    
+    
+    var userData = UserModel()
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func saveButton(_ sender: UIButton) {
+        userData.user = nameTF.text ?? ""
+        userData.coin = 2000
+        userData.score = 0
+        print(userData)
+    }
+    
+    @IBAction func enterButton(_ sender: UIButton) {
+        let myStoryBoard = UIStoryboard.init(name: "GameStoryboard", bundle: nil)
+        let vc = myStoryBoard.instantiateViewController(withIdentifier: "GameVC") as! GameVC
+        vc.userData = userData
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+        print("perehod na GameVC")
+    }
+    
+}
+
+
---
+++ Roulette/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Roulette
+//
+//  Created by Alex on 04.08.2022.
+//
+
+import Foundation
+
+struct UserModel {
+    var user = String()
+    var coin = Int()
+    var score = Int()
+}
